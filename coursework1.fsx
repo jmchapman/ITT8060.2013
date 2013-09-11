fee1af71a0d39dc2de154fc394e0eef51a34746c
James Chapman <jmchapman@gmail.com>
2013-09-11 11:58:53 +0300

    coursework 1 is out

---
+++ coursework1.fsx
@@ -0,0 +1,56 @@
+(*
+
+  ITT8060 -- Advanced Programming 2013
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 1: Basic operations on lists
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+
+
+  Answer the questions below.  You answers to questions 1--8 should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. Please send the completed coursework
+  including your name and student ID in the comments by e-mail to
+  itt8060@cs.ttu.ee by Friday, September 20.
+
+*)
+
+// 1. Make an empty list of generic type.
+
+// 2. Make an empty list of type 'int list' (or list<int>).
+
+// 3. Make a three element list called 'cities' containing pairs of
+// city names (string) and populations (int).
+
+// 4. Write a function 'filterOutVillages' to filter out cities with
+// less than 1000 inhabitants from your list.  It should not use the
+// filter function from the library. The function should use pattern
+// matching and recursion.
+
+// 5. Test the function 'filterOutVillages' on your list 'cities'.
+
+// 6. Calculuate the average population of your list of cities. The
+// function should use pattern matching and recursion.
+
+// 7. Write function 'allcaps' which capitalizes the names of cities in a
+// list of cities. Hint: "abc".ToUpper ()
+
+// 8. Write a function that first filters out villages and then
+// capitalizes names. Use the operation '|>' in your answer.
+
+// 9. Convert 'cities' into a pair of a list of city names and a list
+// of city populations. Use a function that you saw in lecture 2.
+
+// 10. Create a list of pairs of lower case ascii characters (a-z) and
+//     their ascii codes.  e.g. for the letter 'a' the pair would be ('a',97)
+//     Hint: you can use a function you saw in lecture 2 to help.
