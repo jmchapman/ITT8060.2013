da06f97cdf64a6fe96104f8c5e0a2b8c8747a2fa
James Chapman <jmchapman@gmail.com>
2013-09-18 11:50:33 +0300

    Coursework 2 is released. 	new file:   coursework2.data 	new file:   coursework2.fsx

---
+++ coursework2.fsx
@@ -0,0 +1,41 @@
+(*
+
+  ITT8060 -- Advanced Programming 2013
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 2: Using .Net libraries
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+
+
+  Answer the questions below.  You answers to questions 1--8 should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. Please send the completed coursework
+  including your name and student ID in the comments by e-mail to
+  itt8060@cs.ttu.ee by Friday, September 27.
+
+*)
+
+// 1. Write code to load data from the file coursework2.data.
+//    The file is comma separated and contains population data
+//    of towns and communes (vald) in different counties (maakond) of Estonia.
+
+// 2. Make functions that sum the populations of towns and communes
+//    in each county.
+
+// 3. Create a histogram chart where you show the percentages of total
+//    population living in each county.
+
+// 4. Extend the histogram in such a way that each bar is divided into two
+//    parts, representing the appropriate proportion of people living in 
+//    towns and counties.
+
