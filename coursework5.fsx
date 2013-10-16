8f1556133d14a8706506d25aa7ca6d0659419322
James Chapman <jmchapman@gmail.com>
2013-10-16 11:54:27 +0300

    added coursework5

---
+++ coursework5.fsx
@@ -0,0 +1,64 @@
+(*
+
+  ITT8060 -- Advanced Programming 2013
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 5: option, list, generics
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+
+  NB!!! Coursework submission guidelines changed. See below.
+
+  Answer the questions below.  You answers to questions should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. When submitting the coursework,
+  call the file you submit "Lastname_Firstname.fsx" with your first
+  and last name appropriately, attach it to an e-mail with
+  subject line "[ITT8060] Coursework 5", and send it to
+  itt8060@cs.ttu.ee.
+
+  The coursework is due on October 25.
+
+*)
+
+// 1. Implement readAndAdd1 : unit -> option<int>
+//    The function should read 3 numbers from the user, add them
+//    together and return them. In this question use three levels of
+//    pattern matching.
+
+// 2. Implement readAndAdd2 : unit -> option<int>
+//    The function should read 3 numbers from the user,
+//    add them together and return them. In this question do not use
+//    pattern matching, instead use Option.map and Option.bind.
+
+// 3. Write a function optionString : option<int> -> option<string>
+//    You may need to add a type annotation to constrain the type
+
+// 4. Write a function to writelist1 : list<int> -> string
+//    Example input:  > [1..5] |> writelist1;;
+//    Example output: val it : string = "1, 2, 3, 4, 5, "
+//    In this question use pattern matching.
+
+// 5. Write a function to writelist2 : list<int> -> string
+//    Example input:  > [1..5] |> writelist2;;
+//    Example output: val it : string = "1, 2, 3, 4, 5, "
+//    In this question use List.fold.
+
+// 6. ** Bonus **
+
+//   Following the example I wrote on the board today, execute step by
+//   step the result of running readAndAdd3() and and receiving
+//   correct input for the first number but failing input for the
+//   second. Do not skip/combine steps and present you solution in the
+//   same style as I wrote it.
+
+//   You should write you answer below in a comment (* ... *)
