8e787e48d683783c73347530b200ad5d8f143529
Juhan Ernits <juhan.ernits@ttu.ee>
2013-10-30 12:16:07 +0200

    Added code from today's lecture.

---
+++ live9lib.fs
@@ -0,0 +1,63 @@
+﻿namespace Lecture9
+
+
+module myFunctions = 
+  let getLongestBroken (names: list<string>) =
+    names |> List.maxBy (fun name -> name.Length)
+
+  let getLongest (names: list<string>) =
+    match names with
+    | [] -> ""
+    | _ -> names |> List.maxBy (fun name -> name.Length)
+
+  let partitionMultiWord (names: list<string>) =
+    names |> List.partition (fun name -> name.Contains(" "))
+
+
+open Xunit
+
+module LongestTests =   
+
+   [<Fact>]
+   let longestOfNonEmpty() =
+     let test = ["Aaaa"; "Bbbbb"; "Cccc"]
+     Assert.Equal<string>("Bbbbb", myFunctions.getLongest(test))
+
+   [<Fact>]
+   let longestFirstLongest() =
+     let test = ["Aaa"; "Bbb"]
+     Assert.Equal<string>("Aaa", myFunctions.getLongest(test))
+
+   [<Fact>]
+   let longestOfEmpty() =
+     let test = []
+     Assert.Equal<string>("", myFunctions.getLongest(test))
+
+
+ module PartitionTests = 
+
+   [<Fact>]
+   let partitionKeepLength() =
+     let test = ["A"; "A B"; "A B C"; "C"]
+     let multi, single = myFunctions.partitionMultiWord(test)
+     Assert.True(multi.Length + single.Length = test.Length)
+
+   [<Fact>]
+   let partitionNonEmpty() =
+     let test = ["Seattle"; "New York"; "Reading"]
+     let expected = ["New York"], ["Seattle"; "Reading"]
+     Assert.Equal(expected, myFunctions.partitionMultiWord(test))
+
+   [<Fact>]
+   let partitionThenLongest() =
+     let test = ["Seattle"; "New York"; "Grantchester"]
+     let expected = ["New York"], ["Seattle"; "Grantchester"]
+     let actualPartition = myFunctions.partitionMultiWord(test)
+     let actualLongest = myFunctions.getLongest(test)
+
+     Assert.Equal(expected, actualPartition)
+     Assert.Equal<string>("Grantchester", actualLongest)
+
+// fsc /r:path_to_xunit.dll library.fs /t:dll /o:library.dll
+     
+
---
+++ live9script.fsx
@@ -0,0 +1,101 @@
+﻿
+type ClientInfo =
+  { Name: string; Salary: int; CriminalRecord :bool }
+
+type ClientCheck =
+  abstract Check : ClientInfo -> bool
+  abstract Report : ClientInfo -> unit
+
+
+open System 
+open System.Collections.Generic
+
+let noSpaceComparer = 
+  let replace (s: string ) = s.Replace(" ", "")
+  { new IEqualityComparer<_> with
+      member x.Equals(a,b) = 
+        String.Equals(replace(a),replace(b))
+      member x.GetHashCode(s) =
+        replace(s).GetHashCode()
+  }
+
+let scaleNames = new Dictionary<_,_>(noSpaceComparer)
+scaleNames.Add("100", "hundred")
+scaleNames.Add("1 000", "thousand")
+scaleNames.Add("1 000 000", "million")
+
+scaleNames.["1 000"]
+
+scaleNames.["1000000"]
+
+
+open System.IO
+
+let readAndPrint() =
+  let text =
+    use reader = new StreamReader(@"c:\tmp\text.txt")
+    reader.ReadToEnd()
+  Console.Write(text)
+
+readAndPrint()
+
+let changeColor(clr) = 
+  let orig = Console.ForegroundColor
+  Console.ForegroundColor <- clr
+  { new IDisposable with
+      member x.Dispose() =
+        Console.ForegroundColor <- orig 
+  }
+
+
+let hello() =
+  use clr = changeColor(ConsoleColor.Blue)
+  Console.WriteLine("Hello ITT8060!")
+
+hello()
+
+let rnd = new Random()
+
+let rndObject = (rnd :> Object)
+
+//let rnd2 = (rndObject :> Random)
+
+let rnd2 = (rndObject :?> Random)
+
+let rnd3 = (rndObject :?> String)
+
+
+
+#if INTERACTIVE
+#r @"C:\Users\kasutaja\Documents\Visual Studio 2012\Projects\Library2\packages\xunit.1.9.2\lib\net20\xunit.dll"
+#endif
+
+open Xunit
+
+let getLongest (names : list<string>) =
+  names |> List.maxBy (fun name -> name.Length)
+
+let data = ["Aaaa"; "Bbbbb"; "Cccc"]
+
+Assert.Equal<string>("Bbbbb", getLongest(data))
+
+
+type WeatherItem = 
+  { Temperature : int* int;
+    Text        : string }
+
+let winter1 = { Temperature = -10 , -2; Text = "Winter"}
+let winter2 = { Temperature = -10 , -2; Text = "Winter"}
+
+System.Object.ReferenceEquals(winter1,winter2)
+
+winter1.Equals(winter2)
+
+winter1 = winter2
+
+let summer = { Temperature = 10, 20; Text = "Summer" }
+
+summer = winter1
+
+summer > winter1
+
