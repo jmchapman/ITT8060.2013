7d9056e0f16694db4bccffaf3f922b42a50898d2
James Chapman <jmchapman@gmail.com>
2013-11-20 11:54:11 +0200

    added today's lecture notes and coursework 	new file:   coursework9.fsx 	new file:   live12.fsx

---
+++ coursework9.fsx
@@ -0,0 +1,53 @@
+(*
+
+  ITT8060 -- Advanced Programming 2013
+  Department of Computer Science
+  Tallinn University of Technology
+  ------------------------------------
+
+  Coursework 9: Optimizations, Lazyness
+
+  ------------------------------------
+  Name:
+  Student ID:
+  ------------------------------------
+ 
+
+  Answer the questions below.  You answers to questions should be
+  correct F# code written after the question. This file is an F#
+  script file, it should be possible to load the whole file at
+  once. If you can't then you have introduced a syntax error
+  somewhere.
+
+  This coursework will be graded. When submitting the coursework,
+  call the file you submit "Lastname_Firstname.fsx" with your first
+  and last name appropriately, attach it to an e-mail with
+  subject line "[ITT8060] Coursework 9", and send it to
+  itt8060@cs.ttu.ee.
+
+  The coursework is due on December 4.
+
+*)
+
+// 1. Memoize the fibonacci function.
+
+// 2. Define the product of a list of integers in the naive way.
+
+// 3. Define a tail recursive version of product of a list of integers
+
+// 4. Define infinite node labelled trees using Lazy.
+
+// 5. Define map for infinite trees.
+  
+// 6. Make a generator for infinite trees which makes trees like this:
+
+(*
+       n 
+    /     \
+ (n+1)   (n+1)
+  ...     ...
+*)
+// Does your generator duplicate work?
+
+// 7.  Implement flatten from infinite trees to infinite lists
+//     (defined in the lecture).
