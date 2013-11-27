57a274eba8ee966c0ad7e157ced33cbb6821dc58
James Chapman <jmchapman@gmail.com>
2013-11-27 12:15:09 +0200

    first 4 bonus questions of bonus C 	new file:   bonus_courseworkC.fsx

---
+++ bonus_courseworkC.fsx
@@ -0,0 +1,4 @@
+﻿// 1. use the option computation syntax to read three ints and add them together
+// 2. translate this function into the primitive option.Bind and option.Return syntax
+// 3. translate write into primitive log syntax
+// 4. translate read into primitive log syntax
