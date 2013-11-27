dfb585d452713ce413cb291317f81d3136823579
James Chapman <jmchapman@gmail.com>
2013-11-27 12:24:25 +0200

    added a tricky question 5 to bonus coursework C

--- bonus_courseworkC.fsx
+++ bonus_courseworkC.fsx
@@ -2,3 +2,23 @@
 // 2. translate this function into the primitive option.Bind and option.Return syntax
 // 3. translate write into primitive log syntax
 // 4. translate read into primitive log syntax
+// 5. This evaluator threads an environment, hide it using computation expressions:
+
+type Exp = 
+    | Var of string
+    | Val of int
+    | Add of Exp * Exp
+
+type Env = (string * int) list
+
+let rec lookup' s env = 
+    match env with
+    | (x,i)::env -> if x = s then i else lookup' s env
+
+let rec eval' exp env = 
+    match exp with
+    | Var x -> lookup' x env
+    | Val i -> i
+    | Add (e1,e2) -> eval' e1 env + eval' e2 env 
+
+eval' (Add (Var "x",Var "x")) [("x",1)]
